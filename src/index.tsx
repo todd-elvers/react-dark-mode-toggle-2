315b2a4ceb901e97493d72661106da9e6e30c5db
Todd <todd.elvers@gmail.com>
2022-07-25 20:10:14 -0500

    Fixed toggle sometimes showing a flicker of the wrong state

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import LottiePlayer from "react-lottie-player";
+import LottiePlayerLight from "react-lottie-player/dist/LottiePlayerLight";
 import { parseUnit } from "./parseUnit";
 import animationData from "./animationData.json";
 import type { AnimationSegment } from "lottie-web";
@@ -25,8 +25,85 @@ export declare namespace DarkModeToggle {
   };
 }
 
-const buttonStyles = (sizeValue: number, sizeUnit: string): string =>
-  css({
+// TODO: Add ESLint
+export const DarkModeToggle = React.memo<DarkModeToggle.Props>(
+  ({ isDarkMode, onChange, size = 85, speed = 1.3, className = "" }) => {
+    const [sizeValue, sizeUnit] = parseUnit(size);
+    const [segments, setSegments] = React.useState<AnimationSegment>([0, 0]);
+    const [goTo, setGoTo] = React.useState(0);
+
+    // Used to hide the LottiePlayer until it attempts to draw it's first frame
+    const [isLottiePlayerVisible, setLottiePlayerVisible] = React.useState(false);
+
+    // Used to ensure we snap to the initial position of the toggle instead of animating to it
+    const [isReadyToAnimate, setReadyToAnimate] = React.useState(false);
+
+    // Snap the toggle to it's initial position, do not animate to it
+    React.useEffect(() => {
+      setTimeout(() => {
+        setSegments(isDarkMode ? [40, 41] : [0, 1]); // Snap to initial position
+        setReadyToAnimate(true); // Enable animations
+      }, 10);
+    }, []);
+
+    // Re-adjust 'goTo' and 'segments' based on the current state of the toggle
+    React.useEffect(() => {
+      if (!isLottiePlayerVisible) return;
+
+      setGoTo(isDarkMode ? 41 : 0);
+      setSegments(isDarkMode ? [0, 41] : [42, 96]);
+    }, [isLottiePlayerVisible, isDarkMode]);
+
+    return (
+      <button
+        onClick={() => onChange(!isDarkMode)}
+        aria-hidden="true"
+        className={cx(buttonStyles(sizeValue, sizeUnit), className)}
+      >
+        <LottiePlayerLight
+          className={lottieStyles(isLottiePlayerVisible, sizeValue, sizeUnit)}
+          play={isReadyToAnimate}
+          speed={speed}
+          animationData={animationData}
+          loop={false}
+          segments={segments}
+          goTo={goTo}
+          onEnterFrame={() => {
+            setLottiePlayerVisible(true);
+          }}
+        />
+      </button>
+    );
+  },
+  arePropsEqual
+);
+
+DarkModeToggle.displayName = "DarkModeToggle";
+
+function arePropsEqual(prevProps: DarkModeToggle.Props, nextProps: DarkModeToggle.Props) {
+  return (
+    prevProps.size === nextProps.size &&
+    prevProps.isDarkMode === nextProps.isDarkMode &&
+    prevProps.speed === nextProps.speed &&
+    prevProps.className === nextProps.className
+  );
+}
+
+function lottieStyles(isLottieReady: boolean, sizeValue: number, sizeUnit: string): string {
+  return css({
+    // display: "flex",
+    display: isLottieReady ? "flex" : "none",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: `${sizeValue * -0.575}${sizeUnit}`,
+    marginLeft: `${sizeValue * -0.32}${sizeUnit}`,
+    width: `${sizeValue * 1.65}${sizeUnit}`,
+    height: `${sizeValue * 1.65}${sizeUnit}`,
+  });
+}
+
+function buttonStyles(sizeValue: number, sizeUnit: string): string {
+  return css({
     cursor: "pointer",
     overflow: "hidden",
     width: `${sizeValue}${sizeUnit}`,
@@ -36,63 +113,4 @@ const buttonStyles = (sizeValue: number, sizeUnit: string): string =>
     backgroundColor: "transparent",
     padding: 0,
   });
-
-const lottieStyles = (
-  isLottieReady: boolean,
-  sizeValue: number,
-  sizeUnit: string
-): string =>
-  css({
-    display: isLottieReady ? "flex" : "none",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: `${sizeValue * -0.575}${sizeUnit}`,
-    marginLeft: `${sizeValue * -0.32}${sizeUnit}`,
-    width: `${sizeValue * 1.65}${sizeUnit}`,
-    height: `${sizeValue * 1.65}${sizeUnit}`,
-  });
-
-const arePropsEqual = (
-  prevProps: DarkModeToggle.Props,
-  nextProps: DarkModeToggle.Props
-) =>
-  prevProps.size === nextProps.size &&
-  prevProps.isDarkMode === nextProps.isDarkMode &&
-  prevProps.speed === nextProps.speed &&
-  prevProps.className === nextProps.className;
-
-export const DarkModeToggle = React.memo<DarkModeToggle.Props>(
-  ({ isDarkMode, onChange, size = 85, speed = 1.3, className = "" }) => {
-    const [sizeValue, sizeUnit] = parseUnit(size);
-    const [isLottieReady, setLottieReady] = React.useState(false); // Hide the toggle until Lottie is ready
-    const [isReadyToAnimate, setReadyToAnimate] = React.useState(false); // Disable animation of toggle until first click
-
-    const segmentToGoTo: number = isDarkMode ? 51 : 2;
-    const segmentsToPlay: AnimationSegment = isDarkMode ? [2, 50] : [51, 96];
-
-    return (
-      <button
-        onClick={() => {
-          if (!isReadyToAnimate) setReadyToAnimate(true);
-          onChange(!isDarkMode);
-        }}
-        aria-hidden="true"
-        className={cx(buttonStyles(sizeValue, sizeUnit), className)}
-      >
-        <LottiePlayer
-          className={lottieStyles(isLottieReady, sizeValue, sizeUnit)}
-          play={isReadyToAnimate}
-          speed={speed}
-          animationData={animationData}
-          loop={false}
-          segments={segmentsToPlay}
-          goTo={segmentToGoTo}
-          onEnterFrame={() => setLottieReady(true)}
-        />
-      </button>
-    );
-  },
-  arePropsEqual
-);
-
-DarkModeToggle.displayName = "DarkModeToggle";
+}
