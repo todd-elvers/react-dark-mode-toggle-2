7bc8b5b31bfa9e6dfb0ddc0d04bb815bdc446e2c
Todd <todd.elvers@gmail.com>
2022-08-22 00:11:05 -0500

    Clarified purpose of file

--- src/dev.tsx
+++ src/dev.tsx
@@ -3,16 +3,16 @@ import * as ReactDOM from "react-dom/client";
 
 import { DarkModeToggle } from "./index";
 
-/** Internal dev page w/ fast-refresh, run using 'yarn dev' */
+/** Local development page w/ fast-refresh - called when running 'yarn dev' */
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ControlledDarkModeToggle />
+    <LocalDevelopment />
   </React.StrictMode>
 );
 
-function ControlledDarkModeToggle() {
+function LocalDevelopment() {
   const [isDarkMode, setIsDarkMode] = React.useState<boolean>(false);
 
   return (
